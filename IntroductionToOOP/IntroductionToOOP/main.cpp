3fcf16b87b604ebec1fd77496057e9b5c5738b46
Student <Student@201-4>
2019-12-19 20:54:54 +0200

    Contructors

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 //#define BASICS
+//#define ENCAPSULATION
+//#define CONSTRUCTORS
 class Point
 {
 	double x;
@@ -30,9 +32,44 @@ public:
 	{
 		this->y = y;
 	}
+
+	/*
+	Point()
+	{
+		x = y = 0;
+		std::cout << "DefaultConstructor:\t" << this << std::endl;
+
+	}*/
+	Point(double  x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		std::cout << "Constuctor:\t" << this << std::endl;
+	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		std::cout << "CopyConstructor:" << this << std::endl;
+	}
+	~Point()
+	{
+		std::cout << "Destructor:\t" << this << std::endl;
+	}
+	//operators
+	void operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		std::cout << "CopyAssignment:" << this << std::endl;
+	}
+	// Methods
+	void print()const
+	{
+		std::cout << "x= " << x << "\t" << "y = " << y << std::endl;
+	}
 };
 
-#define ENCAPSULATION
 void main()
 {
 	setlocale(LC_ALL, "");
@@ -46,13 +83,43 @@ void main()
 
 	Point* pA = &A;
 	std::cout << pA->x << "\t" << pA->y << std::endl;
-std::cout << (*pA).x << "\t" << (*pA).y << std::endl;
+	std::cout << (*pA).x << "\t" << (*pA).y << std::endl;
 #endif
 
 #ifdef ENCAPSULATION
-Point A;
-A.set_x(5);
-A.set_y(3);
-std::cout << A.get_x() << "\t" << A.get_y() << std::endl;
+	Point A;
+	A.set_x(5);
+	A.set_y(3);
+	std::cout << A.get_x() << "\t" << A.get_y() << std::endl;
 #endif
+#ifdef CONSTRUCTORS
+	const Point B;//Обьявление константного обьекта
+	//std::cout << B.get_x() << std::endl;
+	B.print();
+
+	Point C = 5;
+	C.print();
+
+	Point D(6);
+	D.print();
+
+	Point E(5, 2);
+	E.print();
+
+	Point F = E;
+	F.print();
+
+	Point G;
+	G = E;
+#endif
+
+	Point A(5, 2);
+	A.print();
+	int a = 2;
+	int b = 3;
+	a = b;
+	Point B; //default contructor
+	B = A;//copy assignment
+	B.print();
+
 }
